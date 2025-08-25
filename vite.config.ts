b99cfec3a992bc648590b520ce77fd1f65b326d1
Vitor Renan .bin <vitorrenan2004@gmail.com>
2025-08-25 11:53:20 -0300

    mudanças em vite.cong e package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/SandraModas/" : "/", // 👈 aqui está a mágica
   server: {
     host: "::",
     port: 8080,
